956698faddb9cd06835a6b8d504f0bc244a87ef6
Mritter26 <Ritter.1442@osu.edu>
2014-04-09 19:44:00 -0400

    I added validation to users and passwords in the User controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
 
     validates :userid, presence:true, length: { minimum: 3}
     validates :password, presence:true, length: {minimum: 5, maximum: 18}
+    validates_uniqueness_of :userid
 end
