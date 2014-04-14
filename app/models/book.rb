e47d5f27c61cab842e4b79efec8daedf3d859753
Mritter26 <Ritter.1442@osu.edu>
2014-04-14 16:14:44 -0400

    Began book model validations and methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,30 @@
 class Book < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => true, length: {minimum: 2}
+  validates :author, :presence => true, length: {minimum: 2, maximum: 35}
+
+  def has_name_and_author
+    if self.name == ""
+      return false
+    elsif self.author == ""
+      return false
+    end
+    return true
+  end
+
+  def already_exists(name_or_isbn, book_author)
+    book = Book.find_by_name(name_or_isbn)
+    if !book
+      book = Book.find_by_isbn(name_or_isbn)
+    end
+    if book
+      if book.author == book_author
+        return 1
+      else
+        return 0
+      end
+    else
+      return -1
+    end
+  end
+
 end
