6e023b7167c8a7d4e8d066bdb9c537d4729d098a
Zack Nagaich <vengeance10@gmail.com>
2014-04-14 20:01:05 -0400

    updated session with matt's session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,18 @@
 class SessionsController < ApplicationController
 
-    @@attempts = 0
+   @@attempts = 0
 
     def login
     end
-    
-    
+
     def create
         user = User.find_by(userid: params[:session][:username])
         if user && user.password == params[:session][:password]
-<<<<<<< HEAD
             session[:remember_token] = user.id
-            redirect_to :actiom => 'users/profile'
-        else
-            flash.now[:error] = 'Invalid username or password'
-            redirect_to :actiom => 'users/signup'
-=======
-        	session[:remember_token] = user.id
             @@attempts = 0
             redirect_to action: 'profile', controller: 'users'
         else
-        	flash[:notice] = 'Invalid username or password'
+            flash[:notice] = 'Invalid username or password'
           @@attempts += 1
           if @@attempts == 3
             flash[:notice] = 'That acccount information wasn\'t found. Create an account!'
@@ -31,7 +23,6 @@ class SessionsController < ApplicationController
           end
           render 'sessions/login'
           flash[:notice] = ""
->>>>>>> 12707fe228cbe492d33ebb5904984fba5217402a
         end
     end
 
